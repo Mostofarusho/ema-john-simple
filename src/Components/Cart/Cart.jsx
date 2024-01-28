32e239abf5e0da8eee2c057023fbbaf77445d004
MostofaFattha <mostofacse14@gmail.com>
2024-01-28 12:32:49 +0600

    Show Data From cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -3,19 +3,25 @@ import './Cart.css';
 const Cart = ({cart}) => {
     // const cart=props.cart;
     // const {cart}=props;
-    console.log(cart);
+    // console.log(cart);
     let totalPrice=0;
     let totalShipping=0;
+    let quantity=0;
     for(const product of cart){
-        totalPrice=totalPrice+product.price;
+        // if(product.quantity==0){
+        //     product.quantity=1;
+        // }
+        // product.quantity=product.quantity|| 1;
+        totalPrice=totalPrice+product.price * product.quantity;
         totalShipping=totalShipping+product.shipping;
+        quantity=quantity+product.quantity;
     }
     const tax=(totalPrice*7)/100;
     const grandTotal=totalPrice+totalShipping+tax;
     return (
         <div className='cart'>
             <h4>Orders Summary</h4>
-            <p>Selected Items:{cart.length}</p>
+            <p>Selected Items:{quantity}</p>
             <p>Total Price:${totalPrice}</p>
             <p>Total shipping:{totalShipping}</p>
             <p>Tax:${tax.toFixed(2)}</p>
